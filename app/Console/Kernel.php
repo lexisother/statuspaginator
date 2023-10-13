60e83fd624c106a5b5cf12dbfc35b32d65ee9925
Alyxia Sother <keanu@brik.digital>
2023-10-13 16:41:16 +0200

    [Console/Kernel] Add a note about the plans for the scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // TODO: Write a Job that goes over all configured URLs and fetches version data
+        // $schedule->exec('echo "hiii" > test.txt')->everySecond();
     }
 
     /**
