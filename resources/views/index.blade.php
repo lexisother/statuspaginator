36c4da5fdec336c4afafbf5f294fde0727df177d
Alyxia Sother <keanu@brik.digital>
2023-10-17 21:58:02 +0200

    [views/index] Only show item if data is present

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,20 @@
 @section('content')
     <div class="grid grid-cols-1 gap-4 mx-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
         @foreach($sites as $site)
-            <div class="bg-cardbg border-black border shadow-md p-2 min-h-[9.5rem] grid">
-                <div class="flex flex-col row-start-1 col-start-1">
-                    <div class="text-lg">{{ $site->name }}</div>
-                    <div class="text-sm">{{ $site->url }}</div>
+            @if($site->data)
+                <div class="bg-cardbg border-black border shadow-md p-2 min-h-[9.5rem] grid">
+                    <div class="flex flex-col row-start-1 col-start-1">
+                        <div class="text-lg">{{ $site->name }}</div>
+                        <div class="text-sm">{{ $site->url }}</div>
+
+                        <div class="flex-1"></div>
+
+                        <div class="bg-cardbgalt h-12 -m-2 mt-4 text-sm flex items-center p-2">
+                            <p class="inline">Craft {{ $site->data['craft']['version'] }}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            @endif
         @endforeach
     </div>
 @endsection
