7880a55d916d7296ce87fd8dc39522573f7902ee
Alyxia Sother <keanu@brik.digital>
2023-10-23 15:52:08 +0200

    [views/site] Add timezone and link to frontend

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div class="bg-cardbg border-black border shadow-md m-4 min-h-[9.5rem] grid">
+        {{-- Header --}}
         <div class="m-10 text-center flex flex-row justify-between items-center">
             <div class="flex flex-row gap-4">
                 <h1 class="text-5xl text-hero font-light">{{ $site->name }}</h1>
@@ -13,9 +14,21 @@
                     href="{{ $site->data['meta']['cpurl'] }}"
                     target="_blank"
                 >Control Panel</a>
+                <a
+                    class="bg-blurple py-1 px-6 hover:cursor-pointer"
+                    href="{{ $site->url }}"
+                    target="_blank"
+                >Public Site</a>
             </div>
         </div>
 
+        {{-- Metadata --}}
+        <div class="flex flex-col gap-2 mx-10 mb-10">
+            <h1 class="text-3xl mb-2">Info</h1>
+            <p>Timezone: {{ $site->timezone }}</p>
+        </div>
+
+        {{-- Updates --}}
         <div class="flex flex-col mx-10 mb-10">
             <h1 class="text-3xl mb-2">Updates</h1>
             @if($site->updateAvailable)
