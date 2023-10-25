5333baad79d6b730afeeb438b41954deb638e15f
Alyxia Sother <keanu@brik.digital>
2023-10-25 12:27:24 +0200

    [Providers/OhDear] Make deferrable

--- app/Providers/OhDearProvider.php
+++ app/Providers/OhDearProvider.php
@@ -2,14 +2,29 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 use OhDear\PhpSdk\OhDear;
 
-class OhDearProvider extends ServiceProvider
+class OhDearProvider extends ServiceProvider implements DeferrableProvider
 {
-    public function boot() {
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
         $token = env('OHDEAR_TOKEN');
         $ohDear = new OhDear($token);
         $this->app->instance(OhDear::class, $ohDear);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array<int, string>
+     */
+    public function provides(): array
+    {
+        return [OhDear::class];
     }
 }
