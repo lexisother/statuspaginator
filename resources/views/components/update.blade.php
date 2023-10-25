90467b19f1eb7483958329ea51e642e521f6b4df
Alyxia Sother <keanu@brik.digital>
2023-10-25 12:02:54 +0200

    [components/update] Date and notes can be missing

--- resources/views/components/update.blade.php
+++ resources/views/components/update.blade.php
@@ -6,9 +6,13 @@
 >
     <div class="flex flex-row gap-4 cursor-pointer" @click="opened = !opened">
         <h2 class="text-xl">{{ $update['version'] }}</h2>
-        <h2 class="text-xl text-subtitle">{{ Date::make($update['date'])->toDateString() }}</h2>
+        @if($update['date'])
+            <h2 class="text-xl text-subtitle">{{ Date::make($update['date'])->toDateString() }}</h2>
+        @endif
     </div>
-    <div class="updateContainer ml-8 pt-2" x-cloak x-show="opened">
-        {!! $update['notes'] !!}
-    </div>
+    @if($update['notes'])
+        <div class="updateContainer ml-8 pt-2" x-cloak x-show="opened">
+            {!! $update['notes'] !!}
+        </div>
+    @endif
 </div>
