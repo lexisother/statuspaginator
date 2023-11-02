7485a749c0e0afb4c1dbe745901a4f8686f6b007
Alyxia Sother <keanu@brik.digital>
2023-11-02 10:49:21 +0100

    [Commands/CreateUser] Allow creating as admin

--- app/Console/Commands/CreateUser.php
+++ app/Console/Commands/CreateUser.php
@@ -7,6 +7,7 @@ use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
 
 class CreateUser extends Command implements PromptsForMissingInput
 {
@@ -38,12 +39,16 @@ class CreateUser extends Command implements PromptsForMissingInput
             die(1);
         }
 
+        /** @var User $user */
         $user = User::create([
             'name' => $name,
             'email' => $email,
             'password' => Hash::make($password)
         ]);
         $user->save();
+
+        if ($this->confirm('Make this user an admin?'))
+            $user->assignRole('admin');
 
         $this->info('User successfully created!');
         $this->comment('Do note, the password is temporary and randomly generated! Change it ASAP!');
