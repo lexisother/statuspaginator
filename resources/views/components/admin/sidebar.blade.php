6f767314d544df32bff29de2d07f81a0dc7a5675
Alyxia Sother <keanu@brik.digital>
2023-11-03 16:16:57 +0100

    [components/admin/sidebar] Fix routes

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -10,17 +10,17 @@
 <div class="bg-cardbgalt border border-black flex flex-col p-4 gap-2">
     <div class="flex flex-col">
         <h1 class="text-xl">Administration</h1>
-        <a href="/admin/dashboard/users" class="border-l-2 px-1 {{ activeOrNot('admin.users') }}">
+        <a href="/admin/users" class="border-l-2 px-1 {{ activeOrNot('admin.users') }}">
             <span>Users</span>
         </a>
-        <a href="/admin/dashboard/roles" class="border-l-2 px-1 {{ activeOrNot('admin.roles') }}">
+        <a href="/admin/roles" class="border-l-2 px-1 {{ activeOrNot('admin.roles') }}">
             <span>Roles</span>
         </a>
     </div>
 
     <div class="flex flex-col">
         <h3 class="text-lg">Site management</h3>
-        <a href="/admin/dashboard/sites" class="border-l-2 px-1 {{ activeOrNot('admin.sites') }}">
+        <a href="/admin/sites" class="border-l-2 px-1 {{ activeOrNot('admin.sites') }}">
             <span>Sites</span>
         </a>
     </div>
