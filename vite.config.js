48bbef771a640b8fa91fcd8d8951351238e079af
Alyxia Sother <alyxia@riseup.net>
2025-03-19 16:35:30 +0100

    [*] Move from Sail to DDEV

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+const port = 3000;
+const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`;
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -8,4 +11,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: port,
+        strictPort: true,
+        origin: origin,
+    },
 });
+
