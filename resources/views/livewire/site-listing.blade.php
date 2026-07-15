5224c9cef75f6205097d86b0a1c79c5dc247edd1
Alyxia Sother <alyxia@riseup.net>
2026-07-15 15:10:59 +0200

    [livewire/site-listing] Allow filtering for Commerce

--- resources/views/livewire/site-listing.blade.php
+++ resources/views/livewire/site-listing.blade.php
@@ -42,7 +42,7 @@
                                     <input x-model="search" type="text" class="border text-sm w-full p-2.5 border-black bg-gray-600 text-white" placeholder="Search...">
                                 </div>
                                 <ul class="h-48 px-3 pb-3 overflow-y-auto text-sm text-gray-700 dark:text-gray-200">
-                                    @foreach(['blitz', 'seomatic', 'sprig', 'campaign'] as $plugin)
+                                    @foreach(['commerce', 'blitz', 'seomatic', 'sprig', 'campaign'] as $plugin)
                                         <li x-show="search === '' || '{{ $plugin }}'.includes(search)">
                                             <div class="flex items-center p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
                                                 <input wire:model.live="plugins" id="checkbox-item-11" type="checkbox" value="{{ $plugin }}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
